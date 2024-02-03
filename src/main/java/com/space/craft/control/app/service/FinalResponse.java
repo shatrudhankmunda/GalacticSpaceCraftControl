cd38bf841b4f865509cee07e0c94c3c43acc7fd4
shatrudhankmunda <ershatrughank18@gmail.com>
2024-02-03 17:14:26 +0530

    Adding a final response class

---
+++ src/main/java/com/space/craft/control/app/service/FinalResponse.java
@@ -0,0 +1,22 @@
+package com.space.craft.control.app.service;
+
+public class FinalResponse {
+	private String position;
+	private char direction;
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public char getDirection() {
+		return direction;
+	}
+
+	public void setDirection(char direction) {
+		this.direction = direction;
+	}
+}
