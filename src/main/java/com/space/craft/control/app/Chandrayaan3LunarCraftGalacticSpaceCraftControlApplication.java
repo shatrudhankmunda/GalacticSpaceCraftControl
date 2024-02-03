c4dcb73bcc439751c88d4bfdc15c9a83a8529264
shatrudhankmunda <ershatrughank18@gmail.com>
2024-02-03 19:55:13 +0530

    main method to test application

--- src/main/java/com/space/craft/control/app/Chandrayaan3LunarCraftGalacticSpaceCraftControlApplication.java
+++ src/main/java/com/space/craft/control/app/Chandrayaan3LunarCraftGalacticSpaceCraftControlApplication.java
@@ -1,13 +1,22 @@
 package com.space.craft.control.app;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.space.craft.control.app.service.FinalResponse;
+import com.space.craft.control.app.service.GalacticSpaceCraftControl;
 
 @SpringBootApplication
 public class Chandrayaan3LunarCraftGalacticSpaceCraftControlApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Chandrayaan3LunarCraftGalacticSpaceCraftControlApplication.class, args);
+		//SpringApplication.run(Chandrayaan3LunarCraftGalacticSpaceCraftControlApplication.class, args);
+		GalacticSpaceCraftControl spacecraft = new GalacticSpaceCraftControl();
+		char[] commands = { 'f', 'r', 'u', 'b', 'l' };
+		FinalResponse processCommands = spacecraft.processCommands(commands);
+		char dir = processCommands.getDirection();
+		System.out.println(dir);
+		String cord = processCommands.getPosition();
+		System.out.println(cord);
 	}
 
 }
