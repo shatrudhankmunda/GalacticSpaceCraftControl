88604471bc72ea0eb70ad0b8fa2175e6ed5c4be8
shatrudhankmunda <ershatrughank18@gmail.com>
2024-02-03 06:09:03 +0530

    initial commit

---
+++ src/main/java/com/space/craft/control/app/Chandrayaan3LunarCraftGalacticSpaceCraftControlApplication.java
@@ -0,0 +1,13 @@
+package com.space.craft.control.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chandrayaan3LunarCraftGalacticSpaceCraftControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chandrayaan3LunarCraftGalacticSpaceCraftControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/space/craft/control/app/Chandrayaan3LunarCraftGalacticSpaceCraftControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.space.craft.control.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chandrayaan3LunarCraftGalacticSpaceCraftControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
